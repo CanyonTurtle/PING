a9928c0a8d13ef0f8d3809446550d3e47252bbc6
Cannon Tuttle <na>
2023-05-04 01:45:49 +0000

    update build script to output ping cartridge name

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
     const mode = b.standardReleaseOptions();
-    const lib = b.addSharedLibrary("cart", "src/main.zig", .unversioned);
+    const lib = b.addSharedLibrary("ping", "src/main.zig", .unversioned);
 
     lib.setBuildMode(mode);
     lib.setTarget(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
